a08932d03ca66d2d926c867eda3932c98b6bf17c
Mark Grevtsov <markgrevtsov2310@gmail.com>
2023-11-13 02:15:12 +0300

    logo fixed

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -5,7 +5,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Image from "react-bootstrap/Image"; // Добавлен импорт компонента Image
-import logo from "./path/to/your/logo.png"; // Замените на путь к вашему логотипу
+import logo from "../../../public/BagTracker.png"; // Замените на путь к вашему логотипу
 import styles from "./NavifationBar.module.css";
 
 const NavigationBar: React.FC = () => {
@@ -14,11 +14,7 @@ const NavigationBar: React.FC = () => {
       <Container>
         <Navbar.Collapse className={styles.collapse}>
           <Navbar.Brand as={Link} to="/" className={styles.navbarBrand}>
-            <Image
-              src={"../../../public/BagTracker.png"}
-              alt="Logo"
-              className={styles.logo}
-            />
+            <Image src={logo} alt="Logo" className={styles.logo} />
             BagTracker
           </Navbar.Brand>
           <Nav className={styles.nav}>
